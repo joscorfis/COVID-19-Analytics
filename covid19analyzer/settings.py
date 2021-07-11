d0a67e3e457dc1152070253f1372ddda3c6a9e56
José María Cornac <56033754+joscorfis@users.noreply.github.com>
2021-07-11 14:28:02 +0200

    Update settings.py

--- covid19analyzer/settings.py
+++ covid19analyzer/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
     }
 }
 
+BASEURL = 'http://covid-19analytics.herokuapp.com'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
