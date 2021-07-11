a6a229c020b9454e154d9f54a2560dc422ddb79e
José María Cornac <joscorfis@alum.us.es>
2021-07-11 11:00:22 +0200

    Task-015: Configuración de Travis

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from main import queries
+import unittest
 
-# Create your tests here.
+class TestCases(TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_get_repositorios_coronavirus_mas_visualizados(self):
+        self.assertEqual(5,5)
